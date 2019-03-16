d019ce4dc5cdb5712af6d4abe5b033b7c357dde0
Martin Cruz <martin.cruz@naranja.com>
2019-03-16 14:32:54 -0300

    new version

--- nmap/scan.sh
+++ nmap/scan.sh
@@ -29,18 +29,19 @@ fi
 #Set timestamp:
 export TIMESTAMP="$(date +"%d%m%H%M")"
 
-#Check packages:
+#Check for required packages:
 if [ -z "$(dpkg-query -l | grep 'nmap')" ]; then
 	apt -y install nmap
 fi
 wait
-if [ -z "$(dpkg-query -l | grep 'masscan')" ]; then
-	apt -y install masscan
-fi
-wait
+#Through vpn connections such as pptp, this is useless...
+#if [ -z "$(dpkg-query -l | grep 'masscan')" ]; then
+	#apt -y install masscan
+#fi
+#wait
 
 #Make temp directory:
-mkdir ./temp
+mkdir ./temp && chown -R 1000:1000 ./temp
 
 #Install NSE Vulners scripts (subshell):
 (
@@ -54,9 +55,11 @@ exit
 
 #Read-in list of CIDR networks from input file:
 for CIDR in $(cat $1); do
-    #Start masscan:
+    #Start masscan at host level:
 	RESULTS=${CIDR:: -3}
-    masscan --open -p0-65535 --max-rate 10000 -oB ./temp/"$RESULTS".bin $CIDR; masscan --readscan ./temp/"$RESULTS".bin -oL ./temp/"$RESULTS".txt && \
+    #masscan --open -p0-65535 --max-rate 5000 -oB ./temp/"$RESULTS".bin $CIDR; masscan --readscan ./temp/"$RESULTS".bin -oL ./temp/"$RESULTS".txt && \
+	#Start masscan at docker runtime level (use with virtual adapters):
+	docker run --rm -v $(pwd)/temp:/home/scan -it ilyaglow/masscan --open -p0-65535 --max-rate 5000 -oB /home/scan/"$RESULTS".bin $CIDR; docker run --rm -v $(pwd)/temp:/home/scan -it ilyaglow/masscan --readscan /home/scan/"$RESULTS".bin -oL /home/scan/"$RESULTS".txt && \
 	#Concatenate results:
 	cat ./temp/"$RESULTS".txt >> ./temp/masscan-output.txt
 done
@@ -81,7 +84,7 @@ if [ ! -s ./temp/masscan-output.txt ]; then
     	IP=$(echo $TARGET | awk -F: '{print $1}');
         PORT=$(echo $TARGET | awk -F: '{print $2}');
         FILENAME=$(echo $IP | awk '{print "nmap_"$1}')
-        nmap -vv --script nmap-vulners -sV --version-intensity 5 -sT -O --osscan-limit --max-rate 10000 -Pn -R -T3 -p $PORT -oX ./temp/"$FILENAME".xml --reason $IP
+        nmap -vv --script nmap-vulners,smb-vuln-ms17-010 -sV --version-intensity 5 -sT --max-rate 5000 -Pn -R -T3 -p $PORT -oX ./temp/"$FILENAME".xml --reason $IP
     done
 fi
 
@@ -108,7 +111,7 @@ rm -rf ./temp
 #Run ELK stack (subshell):
 (
 if [[ -z "$(docker ps -q -f name='kibana')" || -z "$(docker ps -q -f name='elasticsearch')" ]]; then
-	cd ../docker-elk && docker-compose up -d &&	sleep 60
+	cd ../docker-elk && docker-compose up -d &&	sleep 30
 	else
 	exit 0
 fi
@@ -125,23 +128,25 @@ fi
 exit
 )
 
+#Create ES index and override ignore_above mapping:
+if [ ! "$(curl --write-out '%{http_code}' -s -X GET "localhost:9200/nmap-vuln-to-es" -o /dev/null)" == "200" ]; then
+	curl -s -X PUT -H 'Content-Type: application/json' -d @../docker-elk/elasticsearch/template.json -v "localhost:9200/nmap-vuln-to-es"
+fi
+wait
+
 #Ingest Nmap Results (subshell):
 (
 	cd ../docker-elk && docker-compose run ingestor ingest \
 	wait \
+#Delete old XML files to avoid duplications:
 	rm _data/nmap/*.xml
 	exit
 )
 
-#Create nmap-vuln-to-es index:
-if [ "$(curl --write-out '%{http_code}' -s -X GET "localhost:9200/nmap-vuln-to-es" -o /dev/null)" == "404" ]; then
-	curl -s -X PUT "localhost:9200/nmap-vuln-to-es"
-fi
+#Create Kibana dashboard:
+curl -s -X POST -H 'Content-Type: application/json' -H 'kbn-xsrf: true' -d @../docker-elk/kibana/dashboard/dashboard.json "localhost:5601/api/kibana/dashboards/import" && \
 
-#Create dashboard:
-curl -s -X POST -H 'Content-Type: application/json' -H 'kbn-xsrf: true' "localhost:5601/api/kibana/dashboards/import" -d @../docker-elk/kibana/dashboard/dashboard.json && \
-
-#Create index-pattern:
+#Create Kibana index-pattern:
 INDEX_ID="$(grep -B1 'index-pattern' ../docker-elk/kibana/dashboard/dashboard.json | grep '"id"' | sed -e 's/.*: "//' -e 's/",//')" \
 
 generate_post_data()
@@ -151,6 +156,6 @@ generate_post_data()
 EOF
 }
 \
-curl -s -X POST -H 'Content-Type: application/json' -H 'kbn-xsrf: true' "localhost:5601/api/kibana/settings/defaultIndex" -d "$(generate_post_data)"
+curl -s -X POST -H 'Content-Type: application/json' -H 'kbn-xsrf: true' -d "$(generate_post_data)" "localhost:5601/api/kibana/settings/defaultIndex"
 
 exit
